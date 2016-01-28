80df97d51f20fcf514ac6d37aa09cfb188bd8e33
aneesh17 <aneesh17@missionrnd.com>
2016-01-28 12:46:42 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -21,6 +21,87 @@ struct transaction {
 	char description[20];
 };
 
+
+int TenPow(int pow)
+{
+	int index, power = 1;
+	for (index = 0; index < pow; index++)
+		power *= 10;
+	return power;
+}
+
+int IsGreater(char *date1, char* date2)
+{
+	int i, p = 0;
+	int temp1 = 0, temp2 = 0, count = 1;
+	//checking for year
+	for (i = 9, p = 0; i >= 6; i--, p++)
+	{
+		temp1 += (date1[i] - '0') * TenPow(p);
+		temp2 += (date2[i] - '0') * TenPow(p);
+	}
+	//printf("%d,%d", temp1,temp2);
+	if (temp1>temp2)
+		return 1;
+	else if (temp1<temp2)
+		return -1;
+	else
+	{
+		//checking for month
+		for (i = 4, p = temp1 = temp2 = 0; i >= 3; i--, p++)
+		{
+			temp1 += (date1[i] - '0') * TenPow(p);
+			temp2 += (date2[i] - '0') * TenPow(p);
+		}
+		if (temp1>temp2)
+			return 1;
+		else if (temp1<temp2)
+			return -1;
+		else
+		{
+			//checking for date
+			for (i = 1, p = temp1 = temp2 = 0; i >= 0; i--, p++)
+			{
+				temp1 += (date1[i] - '0') * TenPow(p);
+				temp2 += (date2[i] - '0') * TenPow(p);
+			}
+			if (temp1>temp2)
+				return 1;
+			else if (temp1<temp2)
+				return -1;
+			else
+				return 0;
+		}
+	}
+}
+
+
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+
+	if (A == NULL || B == NULL)
+		return NULL;
+	struct transaction *result = (struct transaction*) malloc((ALen+BLen) * sizeof(struct transaction));
+
+	int ResIndex = 0, AIndex = 0, BIndex = 0;
+	while (AIndex<ALen && BIndex < BLen)
+	{
+		if (IsGreater(A[AIndex].date, B[BIndex].date) == 1)
+		{
+			result[ResIndex++] = B[BIndex++];
+		}
+		else
+			result[ResIndex++] = A[AIndex++];
+	}
+	while (AIndex < ALen)
+	{
+		result[ResIndex++] = A[AIndex++];
+	}
+	while (BIndex < BLen)
+	{
+		result[ResIndex++] = B[BIndex++];
+	}
+
+	/*for (ResIndex = 0; ResIndex<5; ResIndex++)
+		printf("%s\n", result[ResIndex].date);*/
+	return result;
 }
