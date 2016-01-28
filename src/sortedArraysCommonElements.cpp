615db81a12ecdc24a477d4cc8ef0e79dc9960a8d
aneesh17 <aneesh17@missionrnd.com>
2016-01-28 11:19:40 +0530

    Submit Commit

--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -22,6 +22,100 @@ struct transaction {
 	char description[20];
 };
 
+
+int ten_Pow(int pow)
+{
+	int index, power = 1;
+	for (index = 0; index < pow; index++)
+		power *= 10;
+	return power;
+}
+
+
+int is_Greater(char *date1, char* date2)
+{
+	int i, p = 0;
+	int temp1 = 0, temp2 = 0, count = 1;
+	//checking for year
+	for (i = 9, p = 0; i >= 6; i--, p++)
+	{
+		temp1 += (date1[i] - '0') * ten_Pow(p);
+		temp2 += (date2[i] - '0') * ten_Pow(p);
+	}
+	//printf("%d,%d", temp1,temp2);
+	if (temp1>temp2)
+		return 1;
+	else if (temp1<temp2)
+		return -1;
+	else
+	{
+		//checking for month
+		for (i = 4, p = temp1 = temp2 = 0; i >= 3; i--, p++)
+		{
+			temp1 += (date1[i] - '0') * ten_Pow(p);
+			temp2 += (date2[i] - '0') * ten_Pow(p);
+		}
+		if (temp1>temp2)
+			return 1;
+		else if (temp1<temp2)
+			return -1;
+		else
+		{
+			//checking for date
+			for (i = 1, p = temp1 = temp2 = 0; i >= 0; i--, p++)
+			{
+				temp1 += (date1[i] - '0') * ten_Pow(p);
+				temp2 += (date2[i] - '0') * ten_Pow(p);
+			}
+			if (temp1>temp2)
+				return 1;
+			else if (temp1<temp2)
+				return -1;
+			else
+				return 0;
+		}
+	}
+}
+
+
+int searchelement(struct transaction *arr, char *key, int len, int low, int high)
+{
+	//if(key<arr[low] || key>arr[high])
+	int temp;
+	temp = is_Greater(key, arr[high].date);
+	//printf("%d,%d",temp1,temp2);
+	if (high >= low)
+	{
+		//printf("high=%d\nlow=%d",high,low);
+		int mid = (low + high) / 2;
+		temp = is_Greater(arr[mid].date, key);
+		if (temp == 0)
+			return mid;
+		else if (temp == 1)
+			searchelement(arr, key, len, low, mid - 1);
+		else
+			searchelement(arr, key, len, mid + 1, high);
+	}
+	else
+		return -1;
+}
+
 struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+
+	if (A == NULL || B == NULL)
+		return NULL;
+	int index, ResIndex = 0;
+	int ResLen = (ALen<BLen) ? ALen : BLen;
+	struct transaction* result = (struct transaction*) malloc(ResLen * sizeof(struct transaction));
+	for (index = 0; index<ALen; index++)
+	{
+		int temp = searchelement(B, A[index].date, BLen, 0, BLen - 1);
+		if (temp != -1)
+		{			
+			result[ResIndex++] = B[temp];
+		}
+	}
+	if (ResIndex == 0)
+		return NULL;
+	return result;
 }
